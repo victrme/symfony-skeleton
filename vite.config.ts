625339097dba59d9a23019fa890a84f3ffc9a9c6
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2026-03-29 22:38:21 +0200

    Much easier vite HMR config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-import fs from "node:fs";
 
 export default defineConfig({
     server: {
         host: '0.0.0.0',
-        cors: true,
-        https: {
-            key: fs.readFileSync('certs/vite.key.pem'),
-            cert: fs.readFileSync('certs/vite.crt.pem'),
-        },
-        watch: {
-            ignored: [
-                '**/node_modules/**',
-                '**/.pnpm-store/**'
-            ]
-        },
     },
     plugins: [
         symfonyPlugin({
