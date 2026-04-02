6811dc583ae012213b6ea1d55d1dced1efa54dcc
Victor Azevedo <victor.azevedo@mail.com>
2026-04-02 18:52:35 +0200

    Replace OxC with Biomejs

--- assets/scripts/hello.ts
+++ assets/scripts/hello.ts
@@ -2,7 +2,7 @@ globalThis.window.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector<HTMLButtonElement>('[data-hello-btn]')
     let counter = 1
 
-    btn?.addEventListener('click', function() {
+    btn?.addEventListener('click', () => {
         btn.textContent = `Clicked ${counter++} times`
     })
 })
