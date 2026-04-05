a4887239e019677cc0f5de371f4aff425f2f1219
Victor Azevedo <victor.azevedo@mail.com>
2026-04-05 17:46:20 +0200

    Composer update

--- migrations/Version202601010000000.php
+++ migrations/Version202601010000000.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260329094156 extends AbstractMigration
+final class Version202601010000000 extends AbstractMigration
 {
     public function getDescription(): string
     {
